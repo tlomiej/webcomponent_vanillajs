84549cea2891953da9f44698ef0c6edd6d04e2ee
Bartłomiej Staroń <bartekstaron@gmail.com>
2022-02-02 16:02:07 +0100

    property/attributes

--- counter.js
+++ counter.js
@@ -1,19 +1,46 @@
 class MyCounter extends HTMLElement {
-    constructor(){
-        super();
-        this.shadow = this.attachShadow({mode: 'open'});
-    }
+  constructor() {
+    super();
+    this.shadow = this.attachShadow({ mode: "open" });
+  }
 
-    connectedCallback() {
-        this.render()
-    }
+  get count() {
+    return this.getAttribute("count");
+  }
 
-    render() {
-        this.shadow.innerHTML = `
+  set count(val) {
+    this.setAttribute("count", val);
+  }
+  static get observedAttributes() {
+    return ["count"];
+  }
+
+  attributeChangedCallback(prop, oldVal, newVal) {
+    if (prop == "count") {
+      this.render();
+      let btn = this.shadow.querySelector("#btn");
+      btn.addEventListener("click", this.increment.bind(this));
+    }
+  }
+
+  increment() {
+    this.count++;
+    console.log("111");
+  }
+
+  connectedCallback() {
+    this.render();
+    let btn = this.shadow.querySelector("#btn");
+    btn.addEventListener("click", this.increment.bind(this));
+  }
+
+  render() {
+    this.shadow.innerHTML = `
         <h1>test</h1>
-        `
-    }
+        ${this.count}
+        <button id="btn">Add + 1</button>
+        `;
+  }
 }
 
-
-customElements.define("my-counter", MyCounter)
+customElements.define("my-counter", MyCounter);
